8a9c38e5617001e6fd733b94847e3ca6b3853d8e
Alex Martin <martin.alex.t+github@gmail.com>
2016-05-12 23:44:21 -0400

    Add Alexa supporting files.

---
+++ intents.js
@@ -0,0 +1,17 @@
+{
+  "intents": [
+    { "intent": "GetActiveRoutesIntent" }, 
+    {
+      "intent": "GetRouteInfoIntent",
+      "slots": [
+        {
+          "name": "identifier",
+          "type": "NUMBER"
+        }
+      ]
+    },
+    { "intent": "HelpIntent" },
+    { "Intent": "StopIntent" },
+    { "Intent": "CancelIntent" }
+  ]
+}
